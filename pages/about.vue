5bc0add2e133521ebf1de48896054820d41d5c51
Manan Rathi <76519771+Manan-Rathi@users.noreply.github.com>
2021-11-25 11:54:10 +0530

    Add files via upload

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>About Dad Jokes</h1>
+        <p>Displays corny dad-jokes</p>
+    </div>
+</template>
+
+<script>
+export default{
+  head() {
+      return{
+          title: "About the app",
+          meta: [
+              {
+                  hid: "description",
+                  name: "desc",
+                  content: "desc"
+              }
+          ]
+      }
+  }     
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Welcome to DadJokes!</h2>
+</template>
+
+<script>
+export default{
+  head() {
+      return{
+          title: "Welcome to the app",
+          meta: [
+              {
+                  hid: "description",
+                  name: "desc",
+                  content: "desc"
+              }
+          ]
+      }
+  }      
+};
+</script>
